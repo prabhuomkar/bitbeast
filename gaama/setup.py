51ccc0a99261e999109bd2b7b4e81177dac10f7d
Omkar Prabhu <prabhuomkar@pm.me>
2022-09-09 22:25:34 +0530

    Release a new version of gaama on PYPI

--- gaama/setup.py
+++ gaama/setup.py
@@ -3,7 +3,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 
-version = "0.1.0"
+version = "0.1.1"
 sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8") 
 package_name = "gaama"
 
