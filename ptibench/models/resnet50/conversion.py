b56b7a6c640c6ef961b6915b4a117d29ed11cf10
Omkar Prabhu <prabhuomkar@pm.me>
2023-01-18 10:07:40 +0530

    working nvidia triton example

--- ptibench/models/resnet50/conversion.py
+++ ptibench/models/resnet50/conversion.py
@@ -5,22 +5,25 @@ import torch
 import torch_tensorrt
 
 
+name = "resnet50"
+
 def save_torchscript(model, input):
+    model.half()
     ts_model = torch.jit.trace(model, torch.randn(input).to(torch.device("cuda")).half())
-    torch.jit.save(ts_model, "efficientnet_b0_ts_model.pt")
+    torch.jit.save(ts_model, f"{name}_ts_model.pt")
 
 def save_trt_torchscript(model, input):
     trt_model = torch_tensorrt.compile(model,
-        inputs=[torch_tensorrt.Input(input)],
-        enabled_precisions={torch.half}
+        inputs=[torch_tensorrt.Input(input, dtype=torch.float32)],
+        enabled_precisions={torch.float32}
     )
-    torch.jit.save(trt_model, "efficientnet_b0_trt_model.pt")
+    torch.jit.save(trt_model, f"{name}_trt_model.pt")
 
 if __name__ == '__main__':
     print(f'torch: {torch.__version__}')
     print(f'torch-tensorrt: {torch_tensorrt.__version__}')
     print(f'cuda: {torch.cuda.is_available()}')
-    model = torch.hub.load("pytorch/vision", "efficientnet_b0", weights="IMAGENET1K_V1")
+    model = torch.hub.load("pytorch/vision", name, weights="IMAGENET1K_V1").eval().to("cuda")
     input = (1, 3, 224, 224)
-    save_torchscript(model, input)
     save_trt_torchscript(model, input)
+    save_torchscript(model, input)
